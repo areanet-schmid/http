e0233bd8d65e37ac0d302c2f6dd79baee7d9a29e
markus.schmid <markus.schmid@area-net.de>
2022-07-12 16:41:28 +0200

    Bugfix: Overwrite default http headers

--- ios/Plugin/CapacitorUrlRequest.swift
+++ ios/Plugin/CapacitorUrlRequest.swift
@@ -113,7 +113,7 @@ public class CapacitorUrlRequest: NSObject, URLSessionTaskDelegate {
     public func setRequestHeaders(_ headers: [String: String]) {
         headers.keys.forEach { (key: String) in
             let value = headers[key]
-            request.addValue(value!, forHTTPHeaderField: key)
+            request.setValue(value!, forHTTPHeaderField: key)
             self.headers[key] = value
         }
     }
